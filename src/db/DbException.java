722f10e70b16670d1ceb12237d2e277a9959f4d5
Davidson-mg <61320971+Davidson-mg@users.noreply.github.com>
2020-03-16 16:40:56 -0300

    Update DbException.java

--- src/db/DbException.java
+++ src/db/DbException.java
@@ -2,7 +2,7 @@
 package db;
 
 
-public class DbException extends RuntimeException { /*Criamos uma classe personalizada para exceção, e ela vai herdar da classe do java
+public class DbException extends RuntimeException { /*Criamos uma classe personalizada para exceção, e ela vai extender a classe do java
     RuntimeException*/
     
     public DbException (String msg){ /*Construtor*/
